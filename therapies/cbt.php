126dadc6884f205246902f395f8d3031c2daa2ce
ribells <bcampbel01@risd.edu>
2019-11-04 15:17:25 -0500

    Changes for Nov 4 web conference
    Changes done in preparation of the Nov 4 web conference

---
+++ therapies/cbt.php
@@ -0,0 +1,44 @@
+<?php
+//require 'config.php';
+$thisPage = "cbt";
+require 'therapypage.php';
+?>
+			<!-- Nav -->
+			<div id="top_menu">
+				<ul>
+					<li><a href="../whatisada.html">What is ADA?</a></li>
+					<li><a href="../whowehelp.html">Who We Help</a></li>
+					<li><a href="../aboutus.html">About Us</a></li>
+					<li><a href="../diagtherapy.html">Useful Info</a></li>
+					<li><a href="../tvwriting.html">TV & Writings</a></li>
+				</ul>
+			</div>
+		</div>
+			
+		<!-- Banner -->
+			<section id="content_banner">
+				<div class="inner">
+					<h1 style="background-color: #1b28ce; color: white;">Cognitive behavioral therapy</h1>
+				</div>
+			</section>
+						
+		<!-- Main -->
+			<section id="main" class="wrapper">
+				<div class="inner">
+					<div class="content">
+						<div class="entry">
+							<p id="firstHeading"><strong><strong><a href="../therindex.html#a"> A </a> <a href="../therindex.html#b"> B </a> <a href="../therindex.html#c"> C </a> <a href="../therindex.html#d"> D </a> <a href="../therindex.html#e"> E </a> <a href="../therindex.html#f"> F </a> <a href="../therindex.html#g"> G </a> <a href="../therindex.html#h"> H </a> <a href="../therindex.html#i"> I </a> <a href="../therindex.html#j">J </a> <a href="../therindex.html#k"> K </a> <a href="../therindex.html#l"> L </a> <a href="../therindex.html#m"> M </a> <a href="../therindex.html#n"> N </a> <a href="../therindex.html#o"> O </a> <a href="../therindex.html#p"> P </a> Q <a href="../therindex.html#r"> R </a> <a href="../therindex.html#s"> S </a> <a href="../therindex.html#t"> T </a> U <a href="../therindex.html#v"> V </a>  W X Y Z</a></strong></strong></p>
+							<?php 
+							    include "cbt.html";
+							?>
+					</div>
+				</div>
+			</section>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+<?php   include ('../includes/endpage.html'); ?>
---
+++ therapies/therapypage.php
@@ -0,0 +1,24 @@
+<?php
+print '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+
+<!DOCTYPE HTML>
+<!--
+	Industrious by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>ADA Therapy LLC | Applied Developmental Therapy</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="../assets/css/main.css" />
+		<link rel="stylesheet" href="../assets/css/selectBoxIt.css" />
+	</head>
+
+	<body class="is-preload">
+<?php
+require '../logo-menu.php';
+?>
