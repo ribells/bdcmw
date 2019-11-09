1e4026d32da46f2240dab45022d239978195a9b6
ribells <bcampbel01@risd.edu>
2019-11-09 10:39:11 -0500

    start adding disorders to site
    start adding disorders to site using screen scraping technique

---
+++ disorders/disorderpage.php
@@ -0,0 +1,36 @@
+<?php
+print '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+
+<!DOCTYPE HTML>
+<!--
+	Industrious by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>ADA Therapy LLC | Applied Developmental Therapy</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="../assets/css/main.css" />
+		<link rel="stylesheet" href="../assets/css/selectBoxIt.css" />
+	</head>
+
+	<body class="is-preload">
+<?php
+require '../logo-menu.php';
+?>
+			<!-- Nav -->
+			<div id="top_menu">
+				<ul>
+					<li><a href="../whatisada.html">What is ADA?</a></li>
+					<li><a href="../whowehelp.html">Who We Help</a></li>
+					<li><a href="../whatwedo.html">What We Do</a></li>
+					<li><a href="../aboutus.html">About Us</a></li>
+					<li><a href="../diagtherapy.html">Useful Info</a></li>
+					<li><a href="../tvwriting.html">TV & Writings</a></li>
+				</ul>
+			</div>
+		</div>
