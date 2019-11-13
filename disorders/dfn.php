6790fcc7e60b0637838ddc7c0b1782c55e9f25da
ribells <bcampbel01@risd.edu>
2019-11-13 15:59:29 -0500

    finish disorders pages

---
+++ disorders/dfn.php
@@ -0,0 +1,32 @@
+<?php
+//require 'config.php';
+$thisPage = "dfn";
+require 'disorderpage.php';
+?>		
+		<!-- Banner -->
+			<section id="content_banner">
+				<div class="inner">
+					<h2 style="background-color: #1b28ce; color: white;">Deafness</h2>
+				</div>
+			</section>
+						
+		<!-- Main -->
+			<section id="main" class="wrapper">
+				<div class="inner">
+					<div class="content">
+						<div class="entry">
+							<p id="firstHeading"><strong><strong><a href="../devindex.html#a"> A </a> B <a href="../devindex.html#c"> C </a> <a href="../devindex.html#d"> D </a> <a href="../devindex.html#e"> E </a> <a href="../devindex.html#f"> F </a> G <a href="../devindex.html#h"> H </a> <a href="../devindex.html#i"> I </a> J  K <a href="../devindex.html#l"> L </a> <a href="../devindex.html#m"> M </a> <a href="../devindex.html#n"> N </a> <a href="../devindex.html#o"> O </a> <a href="../devindex.html#p"> P </a> Q <a href="../devindex.html#r"> R </a> <a href="../devindex.html#s"> S </a> <a href="../devindex.html#t"> T </a> U <a href="../devindex.html#v"> V </a> <a href="../devindex.html#v"> W </a> X Y Z</a></strong></strong></p>
+							<?php 
+							    include "dfn.html";
+							?>
+					</div>
+				</div>
+			</section>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+<?php   include ('../includes/endpage.html'); ?>
